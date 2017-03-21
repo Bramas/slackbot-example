11867e28337c332b3e01b093e0ee2b9001a38e7a
Quentin Bramas <quentin.bramas@gmail.com>
2017-03-21 12:13:00 +0100

    add test

--- index.js
+++ index.js
@@ -27,3 +27,30 @@ app.get('/label', function(req, res) {
 app.listen(app.get('port'), function() { 
        console.log('Node app is running on port', app.get('port'));
  });
+
+var SlackBot = require('slackbots');
+
+// create a bot
+var bot = new SlackBot({
+    token: 'xoxb-157253903859-69pg0ecpMMfjAEY7cDVSTf8S',
+    name: 'Bramas_Bot_2'
+});
+
+bot.on('start', function() {
+
+    bot.postMessageToChannel('general', 'meow!');
+
+    // define existing username instead of 'user_name'
+    bot.postMessageToUser('bramas', 'meow!');
+
+    bot.on('message', function(data) {
+        // all ingoing events https://api.slack.com/rtm
+        console.log(data);
+        if(data.type=='message' && data.channel.charAt(0) == 'D' &&
+        !data.bot_id
+    ) {
+            bot.postMessage(data.channel, 'meow!');
+        }
+    });
+
+});
